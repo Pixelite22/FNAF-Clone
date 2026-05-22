ad939abcb5c78a8294c4c78e1ee75444158a036b
Pixelite22 <114366972+Pixelite22@users.noreply.github.com>
2026-05-22 15:14:51 -0400

    UPDATE 4
    Foxy is the only issue left now.  He's being really dumb.  I gotta figure out why his animation isn't playing.  Taking a break so I can come back with a clear head

--- Scenes and Code/Code/Camera Code/cams.gd
+++ Scenes and Code/Code/Camera Code/cams.gd
@@ -36,7 +36,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 #when a camera button is pressed
